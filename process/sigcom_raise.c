ab9d2aa10dfda3ebf86b9da21bf3c7dbafb92997
panda1950 <panda1950@163.com>
2018-01-30 16:01:50 +0800

    signal communication of raise

---
+++ process/sigcom_raise.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <stdlib.h>
+
+int main()
+{
+	printf("raise before\n");
+	raise(9);
+	printf("raise after\n");
+
+
+	return 0;
+}
+
+
